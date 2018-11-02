23de38a4066cfea0e8375667dd29238917877cd2
Nathan Vogel <nathan@nathanvogel.com>
2018-11-02 11:29:33 -0700

    Working communication

--- nathan/js/example-action.js
+++ nathan/js/example-action.js
@@ -1,11 +1,36 @@
 /* globals moment */
 
+// addEventListener support for IE8
+function bindEvent(element, eventName, eventHandler) {
+  if (element.addEventListener) {
+    element.addEventListener(eventName, eventHandler, false);
+  } else if (element.attachEvent) {
+    element.attachEvent("on" + eventName, eventHandler);
+  }
+}
+
 // Create project iframe
 var iframe = document.createElement("iframe");
 iframe.setAttribute("src", "./project/index.html");
 iframe.style.width = "100%";
 iframe.style.height = "100%";
 // document.body.appendChild(iframe);
+
+// Send a message to the child iframe
+var sendMessage = function(msg) {
+  // Make sure you are sending a string, and to stringify JSON
+  iframe.contentWindow.postMessage(msg, "*");
+};
+// Send random messge data on every button click
+// bindEvent(messageButton, 'click', (e) => {
+//     var random = Math.random();
+//     sendMessage('' + random);
+// });
+
+// Listen to message from child window
+// bindEvent(window, 'message', (e) => {
+//     results.innerHTML = e.data;
+// });
 
 $(document).ready(() => {
   // Add iframe
@@ -14,11 +39,10 @@ $(document).ready(() => {
   mainContainer.appendChild(iframe);
 
   document.addEventListener("timeline-scroll", e => {
-    console.log("[student-action.js] -----callback timeline-scroll");
-
+    // console.log("[student-action.js] -----callback timeline-scroll");
     // let normal = moment(e.detail.normal);
-    let dotDate = moment(e.detail.date);
-    console.log(dotDate.format("DD/MM/YYYY"));
+    // let dotDate = moment(e.detail.date);
+    // console.log(dotDate.format("DD/MM/YYYY"));
     // $("#container-main").html(dotDate.format("DD/MM/YYYY"));
   });
 
@@ -32,5 +56,6 @@ $(document).ready(() => {
     // let dot = $(".timeDot[idToLoad='" + dotID + "']");
     // dot.attr("startdate");
     // console.log("Loaded date : " + dot.attr("startdate"));
+    sendMessage(JSON.stringify({ id: dotID }));
   });
 });
